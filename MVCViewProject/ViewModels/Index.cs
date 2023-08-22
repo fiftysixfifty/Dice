5d02764755e7d09378034fb1457eb65820a97371
Peter Rosario <fiftysixfifty@gmail.com>
2023-08-22 16:29:44 -0500

    MVCViewProject: Added ViewModels/Index and used in Views/Home/Index.

---
+++ MVCViewProject/ViewModels/Index.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCViewProject.ViewModels;
+
+public class Index
+{
+    [RangeAttribute(minimum: 1, maximum: 6)]
+    public byte FirstFavoredFace { get; set; }
+
+    [RangeAttribute(minimum: 1, maximum: 5)]
+    public byte FirstFavorFactor { get; set; }
+
+
+    [RangeAttribute(minimum: 1, maximum: 6)]
+    public byte SecondFavoredFace { get; set; }
+
+    [RangeAttribute(minimum: 1, maximum: 5)]
+    public byte SecondFavorFactor { get; set; }
+
+
+    [RangeAttribute(minimum: 1, maximum: 255)]
+    public byte NumberOfTimesToRoll { get; set; }
+}
