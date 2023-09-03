4b5ae70ec7b15c4beb5aedfa50c9c126afe02164
Peter Rosario <fiftysixfifty@gmail.com>
2023-09-03 18:26:26 -0500

    ModelViewLibrary/Result/Result: Added.

---
+++ ModelLibrary/Result/Result.cs
@@ -0,0 +1,15 @@
+﻿namespace ModelLibrary.Result;
+
+public class Result
+{
+    #region Properties
+    public ModelLibrary.Die.Die Die  { get; }
+    public byte                 Face { get; }
+    #endregion
+
+    public Result(ModelLibrary.Die.Die die): base()
+    {
+        this.Die  = die                                                  ;
+        this.Face = ModelLibrary.Die.Die.ValidFace(face: this.Die.Roll());
+    }
+}
