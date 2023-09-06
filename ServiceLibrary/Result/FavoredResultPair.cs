73362858866090a0e106bfff98d890ceb1d1bb71
Peter Rosario <fiftysixfifty@gmail.com>
2023-09-06 11:41:41 -0500

    ServiceLibrary: Started using ModelLibrary. ServiceLibrary/Result/FavoredResultPair: Added.

---
+++ ServiceLibrary/Result/FavoredResultPair.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceLibrary;
+
+public class FavoredResultPair
+{
+    public ModelLibrary.Result.FavoredResultPairList Add(
+    ModelLibrary.Result.FavoredResultPairList result)
+    {
+        return result;
+    }
+}
