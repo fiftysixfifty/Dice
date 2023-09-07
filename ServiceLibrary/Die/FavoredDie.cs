b9281fcf47981de4b14a27fa8282aefb90a999bf
Peter Rosario <fiftysixfifty@gmail.com>
2023-09-07 10:55:11 -0500

    ServiceLibrary: Started using ModelLibrary. ServiceLibrary/Die/FavoredDie: Added.

---
+++ ServiceLibrary/Die/FavoredDie.cs
@@ -0,0 +1,14 @@
+﻿namespace ServiceLibrary.Die;
+
+internal static class FavoredDie
+{
+    internal static int Add(
+    StorageLibrary.DbContext    dbContext ,
+    ModelLibrary.Die.FavoredDie favoredDie)
+    {
+        dbContext.FavoredDieDbSet.Add(entity: favoredDie);
+        dbContext.SaveChanges();
+
+        return favoredDie.FavoredDieId;
+    }
+}
